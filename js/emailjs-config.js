4b759c8a4a224477e503f07fd81d308954d44e1b
pcbsystempr <pcbsystem.pr@gmail.com>
2026-04-16 21:50:50 -0400

    Add files via upload

--- js/emailjs-config.js
+++ js/emailjs-config.js
@@ -4,43 +4,43 @@ const emailjsConfig = {
         serviceID: 'service_jwkxgrm',
         templateID: 'template_gaqvy95',
         publicKey: 'sUT_mezeXvnOxkvVI',
-        toEmail: 'soniaconsejeravoc@gmail.com'
+        toEmail: 'de58390@miescuela.pr'
     },
     maricarmen: {
         serviceID: 'service_tl36rip',
         templateID: 'template_pck97ew',
         publicKey: 'SCqXLNS1v8MCpsAit',
-        toEmail: 'maricarmenpcb@gmail.com'
+        toEmail: 'de85494@miescuela.pr'
     },
     karem: {
         serviceID: 'service_3xbuvck',
         templateID: 'template_prewukp',
         publicKey: 'JEnP4Ol_npOF0u1zF',
-        toEmail: 'karemzita78@gmail.com'
+        toEmail: 'de156358@miescuela.pr'
     },
     janelys: {
         serviceID: 'service_kkgfbkh',
         templateID: 'template_8xrudkm',
         publicKey: 'X-1xy2n2m5N47ACld',
-        toEmail: 'janelyzts@gmail.com'
+        toEmail: 'de167251@miescuela.pr'
     },
     karem_ts: {
         serviceID: 'service_3xbuvck',
         templateID: 'template_prewukp',
         publicKey: 'JEnP4Ol_npOF0u1zF',
-        toEmail: 'karemzita78@gmail.com'
+        toEmail: 'de156358@miescuela.pr'
     },
     janelys_ts: {
         serviceID: 'service_kkgfbkh',
         templateID: 'template_8xrudkm',
         publicKey: 'X-1xy2n2m5N47ACld',
-        toEmail: 'janelyzts@gmail.com'
+        toEmail: 'de167251@miescuela.pr'
     },
     psicologo: {
         serviceID: 'service_tsl1ikj',
         templateID: 'template_r9ru3zj',
         publicKey: '0fOfcEG1uHW5tB2Nb',
-        toEmail: 'jesuspsicologo9@gmail.com'
+        toEmail: 'de161266@miescuela.pr'
     }
 };
 
